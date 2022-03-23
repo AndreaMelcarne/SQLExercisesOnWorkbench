73ff71e020b3fcfb0c94395697188f3f3a36b87f
AndreaMelcarne <49690061+AndreaMelcarne@users.noreply.github.com>
2022-03-23 17:55:10 +0100

    Update TASK 7.sql

--- TASK 7.sql
+++ TASK 7.sql
@@ -1,5 +1,5 @@
 SELECT orderdate,
 orderdetails.productcode
 FROM orders
-INNER JOIN orderdetails ON orderdetails.orderNumber=orders.orderNumber
+LEFT OUTER JOIN orderdetails ON orderdetails.orderNumber=orders.orderNumber
 WHERE EXTRACT(YEAR FROM orderdate) NOT BETWEEN "2003" AND "2005"
