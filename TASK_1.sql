b6d23354152cdd6e936de1117d09b1a99b9ceb20
AndreaMelcarne <49690061+AndreaMelcarne@users.noreply.github.com>
2022-03-31 15:07:06 +0200

    Update TASK_1.sql

--- TASK_1.sql
+++ TASK_1.sql
@@ -1,3 +1,5 @@
+-- The VP of sales would like to award EOY bonues to The employee in the country who has recorded the most sales in the three year period
+
 SELECT C.salesRepEmployeeNumber as Employee_ID, 
         COUNT(O.orderNumber) as Num_of_orders_per_employee
 		FROM orders O
